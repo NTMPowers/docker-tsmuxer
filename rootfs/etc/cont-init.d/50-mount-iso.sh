52a0e45c0b6b405c706938718a277ea37f596d65
NTM <NTMPowers@users.noreply.github.com>
2026-09-10 07:34:50 +0200

    Enhance 50-mount-iso.sh to add ISO bookmarks
    Added functionality to automatically bookmark the mounted ISO in GTK file dialogs.

--- rootfs/etc/cont-init.d/50-mount-iso.sh
+++ rootfs/etc/cont-init.d/50-mount-iso.sh
@@ -13,6 +13,16 @@ if [ -n "${ISO_NAME:-}" ]; then
         echo "Mounting '$ISO_PATH' to /iso..."
         mount -t udf,iso9660 -o ro,loop "$ISO_PATH" /iso
         echo "Successfully mounted to /iso"
+
+        # --- Automatically add /iso to the file dialog sidebar bookmarks ---
+        BOOKMARK_DIRS="/config/.config/gtk-3.0 /config/xdg/config/gtk-3.0 /root/.config/gtk-3.0"
+        for DIR in $BOOKMARK_DIRS; do
+            mkdir -p "$DIR"
+            if ! grep -qs "file:///iso" "$DIR/bookmarks" 2>/dev/null; then
+                echo "file:///iso iso" >> "$DIR/bookmarks"
+            fi
+        done
+        # ------------------------------------------------------------------
     else
         echo "WARNING: '$ISO_PATH' not found! Skipping mount." >&2
     fi
